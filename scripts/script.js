b61112142031ef09b6aff1ec21e2c6c55abf4e3f
Анастасия Подкопаева <testsolnishshka@yandex.ru>
2020-09-04 08:17:29 +0300

    feat: Добавлен скрипт

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,11 @@
-let editButton = document.querySelector('.profile__edit-button');
+const editButton = document.querySelector('.profile__edit-button');
+const popup = document.querySelector('.popup');
+const closeButton = document.querySelector('.popup__close-button');
+
+const popupTogle = function() {
+    popup.classList.toggle('popup_opened');
+}
+
+editButton.addEventListener('click', popupTogle);
+closeButton.addEventListener('click', popupTogle);
+
