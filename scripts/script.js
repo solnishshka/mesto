28741e91b6d2f4fca69e2c48c87d183174a9bae6
Анастасия Подкопаева <testsolnishshka@yandex.ru>
2020-09-05 08:07:13 +0300

    feat: Черновой вариант проекта

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,36 @@
+const allPage = document.querySelector('.page');
 const editButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const closeButton = document.querySelector('.popup__close-button');
+let formElement = document.querySelector('.form');
+
+console.log(allPage);
 
 const popupTogle = function() {
     popup.classList.toggle('popup_opened');
+    allPage.classList.toggle('page_js-overflow');
 }
 
 editButton.addEventListener('click', popupTogle);
 closeButton.addEventListener('click', popupTogle);
+
+function formSubmitHandler (evt) {
+    evt.preventDefault(); 
+
+    let nameInput = formElement.querySelector('.form__item_el_name');
+    let jobInput = formElement.querySelector('.form__item_el_position');
+
+    nameInput.value;
+    jobInput.value;
+
+    let profileTitle = document.querySelector('.profile__title');
+    let profileSubtitle = document.querySelector('.profile__subtitle'); 
+
+    profileTitle.textContent = nameInput.value;
+    profileSubtitle.textContent = jobInput.value;
+
+    popupTogle();
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
 
