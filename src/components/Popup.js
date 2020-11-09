428ed42a950614ffba0187640bb2e0f4de56f082
solnishshka <testsolnishshka@yandex.ru>
2020-11-09 21:32:35 +0300

    bugfix: fix gitignore

---
+++ src/components/Popup.js
@@ -0,0 +1,32 @@
+export default class Popup {
+  constructor(popupSelector) {
+    this._popup = document.querySelector(popupSelector);
+  }
+
+  open() {
+    this._popup.classList.add("popup_opened");
+    document.addEventListener("keyup", this._handleEscClose.bind(this));
+  }
+
+  close() {
+    this._popup.classList.remove("popup_opened");
+    document.removeEventListener("keyup", this._handleEscClose.bind(this));
+  }
+
+  _handleEscClose(evt) {
+    if (evt.key === "Escape") {
+      this.close();
+    }
+  }
+
+  setEventListeners() {
+    this._popup.addEventListener("click", (evt) => {
+      if (
+        evt.target.classList.contains("popup") ||
+        evt.target.classList.contains("popup__close-button")
+      ) {
+        this.close();
+      }
+    });
+  }
+}
---
+++ src/components/Section.js
@@ -0,0 +1,17 @@
+export default class Section {
+  constructor({ data, renderer }, containerSelector) {
+    this._data = data;
+    this._renderer = renderer;
+    this._container = document.querySelector(containerSelector);
+  }
+
+  renderItems() {
+    this._data.forEach((item) => {
+      this._renderer(item);
+    });
+  }
+
+  addItem(element) {
+    this._container.prepend(element);
+  }
+}
